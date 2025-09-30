1b3da16770ea1c1f17254e238d7b98cdc651d9a6
ogjex <m.agnu.ha@gmail.com>
2025-09-30 06:37:03 +0200

    re-update all files with own files

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER " | "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
@@ -18,15 +18,16 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("", "sb-mail", 600, 1)  \
-    X("", "sb-music", 0, 2)   \
-    X("", "sb-disk", 1800, 3) \
+    X("", "sb-nettraf", 5, 1) \
+   X("", "sb-internet", 5, 2) \
+    X("", "sb-volume", 0, 3)  \
+    X("", "sb-battery", 5, 4) \
+    X("", "sb-clock", 45, 5)
+
+#endif  // CONFIG_H
+/*    X("", "sb-disk", 1800, 3) \
     X("", "sb-memory", 10, 4) \
     X("", "sb-loadavg", 5, 5) \
     X("", "sb-mic", 0, 6)     \
-    X("", "sb-record", 0, 7)  \
-    X("", "sb-volume", 0, 8)  \
-    X("", "sb-battery", 5, 9) \
-    X("", "sb-date", 1, 10)
-
-#endif  // CONFIG_H
+    X("", "sb-record", 0, 7)  \ */
+ 
